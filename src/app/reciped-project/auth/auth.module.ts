1e3053aab2c68b66b5bb8393a602bd5e618ef35a
checkfelix123 <42866735+checkfelix123@users.noreply.github.com>
2020-02-09 21:41:40 +0100

    created new branch develop

--- src/app/reciped-project/auth/auth.module.ts
+++ src/app/reciped-project/auth/auth.module.ts
@@ -2,7 +2,6 @@ import {NgModule} from '@angular/core';
 import {SharedModule} from '../shared/shared.module';
 import {AuthComponent} from './auth.component';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {CommonModule} from '@angular/common';
 
 @NgModule({
   declarations: [AuthComponent],
